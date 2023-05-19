88e32447ae01ede3119c9e3791b470a1e5ffa414
marj28 <honeymarjey@gmail.com>
2023-05-19 14:33:53 +0800

    Add Toolbar each user

--- src/components/core/Toolbar.vue
+++ src/components/core/Toolbar.vue
@@ -1,111 +1,9 @@
 <template>
   <div>
-    <v-toolbar dense elevation="1">
-      <img :src="$store.state.systemlogo" class="mr-2" width="40px" />
-      <v-toolbar-items class="hidden-md-and-down">
-        <v-btn text tile @click="$router.push('/home').catch((err) => {})">
-          Home
-        </v-btn>
-        <v-btn text tile> What is PEESO? </v-btn>
-        <!-- <v-btn text tile @click="$router.push('/home').catch((err) => {})">
-          Company Profiles
-        </v-btn> -->
-        <v-btn text tile @click="$router.push('/home').catch((err) => {})">
-          Contact Us
-        </v-btn>
-      </v-toolbar-items>
-      <v-menu >
-        <!-- Sa icon diay ibutang ang class para ma-hide -->
-        <template v-slot:activator="{ on, attrs }">
-          <v-icon color="primary" dark v-bind="attrs" v-on="on" class="hidden-lg-and-up">
-            mdi-menu
-          </v-icon>
-        </template>
-        <v-list>
-          <v-list-item @click="$router.push('/home').catch((err) => {})">
-            <v-list-item-title>HOME</v-list-item-title>
-          </v-list-item>
-          <v-list-item>
-            <v-list-item-title>WHAT IS PEESO?</v-list-item-title>
-          </v-list-item>
-          <!-- <v-list-item @click="$router.push('/home').catch((err) => {})">
-            <v-list-item-title>COMPANY PROFILES</v-list-item-title>
-          </v-list-item> -->
-          <v-list-item @click="$router.push('/home').catch((err) => {})">
-            <v-list-item-title>CONTACT US</v-list-item-title>
-          </v-list-item>
-        </v-list>
-      </v-menu>
-      <v-spacer />
-      <template v-if="isLoggin">
-        <v-menu offset-y>
-          <template #activator="{ on, attrs }">
-            <span class="caption text-primary" v-bind="attrs" v-on="on"
-              ><v-icon left>mdi-account-circle</v-icon> My Account</span
-            >
-          </template>
-          <v-list dense>
-            <template v-for="(menu, index) in MENUS">
-              <v-list-item
-                :key="index"
-                @click="$router.push(menu.route).catch((err) => {})"
-              >
-                <v-list-item-title>
-                  <v-icon left>
-                    {{ menu.icon }}
-                  </v-icon>
-                  {{ menu.label }}
-                </v-list-item-title>
-              </v-list-item>
-            </template>
-            <v-list-item @click="confirm = true" color="warning">
-              <v-list-item-title>
-                <v-icon left color="error"> mdi-power </v-icon>Logout
-              </v-list-item-title>
-            </v-list-item>
-          </v-list>
-        </v-menu>
-      </template>
-      <template v-else>
-        <v-btn
-          small
-          text
-          class="caption text-primary"
-          color="warning"
-          @click="$router.push('/register').catch((err) => {})"
-        >
-          Create an Account
-        </v-btn>
-        <v-divider vertical inset />
-        <v-btn
-          small
-          text
-          class="caption text-primary"
-          color="success"
-          @click="$router.push('/login').catch((err) => {})"
-        >
-          LOGIN
-        </v-btn>
-      </template>
-      <v-dialog v-model="confirm" persistent max-width="290">
-        <v-card color="#232323" dark>
-          <v-card-text class="pt-5"> Continue log out? </v-card-text>
-          <v-card-actions>
-            <v-btn color="warning darken-1" text @click="confirm = false">
-              Cancel
-            </v-btn>
-            <v-spacer />
-            <v-btn
-              color="green darken-1"
-              text
-              @click="(confirm = false), logout()"
-            >
-              Yes
-            </v-btn>
-          </v-card-actions>
-        </v-card>
-      </v-dialog>
-    </v-toolbar>
+    <core-student-toolbar v-if= "student" />
+    <core-employer-toolbar v-if="employer" />
+    <core-applicant-toolbar v-if="applicant" />
+    <core-admin-toolbar v-if="admin" />
   </div>
 </template>
 
@@ -113,100 +11,50 @@
 import { mapMutations } from "vuex";
 export default {
   data: () => ({
+    student: false,
+    employer: false,
+    applicant: false,
+    admin: false,
     confirm: false,
     listing: false,
     // title: null,
-    seeker_menu: [
-      {
-        label: "My Account",
-        icon: "mdi-card-account-details",
-        route: "/profile",
-      },
-      {
-        label: "Jobs",
-        icon: "mdi-account-hard-hat",
-        route: "/jobview",
-      },
-      {
-        label: "Trainings",
-        icon: "mdi-forum",
-        route: "/trainingview",
-      },
-    ],
-
-    student_menu: [
-      {
-        label: "My Account",
-        icon: "mdi-card-account-details",
-        route: "/studentprofile",
-      },
-      {
-        label: "Programs",
-        icon: "mdi-note-plus",
-        route: "/programview",
-      },
-      {
-        label: "Trainings",
-        icon: "mdi-forum",
-        route: "/trainingview",
-      },
-      {
-        label: "Jobs",
-        icon: "mdi-account-hard-hat",
-        route: "/jobview",
-      },
-    ],
-
-    employeer_menu: [
-      {
-        label: "My Company",
-        icon: "mdi-card-account-details",
-        route: "/employeer",
-      },
-      {
-        label: "Jobs",
-        icon: "mdi-account-hard-hat",
-        route: "/job-post",
-      },
-    ],
-
-    admin_menu: [
-      {
-        label: "My Account",
-        icon: "mdi-card-account-details",
-        route: "/admin",
-      },
-      {
-        label: "Jobs",
-        icon: "mdi-account-hard-hat",
-        route: "/job-post",
-      },
-      {
-        label: "Trainings",
-        icon: "mdi-forum",
-        route: "/training-details",
-      },
-      {
-        label: "Programs",
-        icon: "mdi-note-plus",
-        route: "/program-page",
-      },
-    ],
   }),
-
+  created() {
+    // console.log("role=",this.$session.user.role)
+    if (this.user.role == "Student") {
+      this.student = true;
+    } else if (this.user.role == "Employeer") {
+      this.employer = true;
+    } else if (this.user.role == "Jobseeker") {
+      this.applicant = true;
+    } else if (this.user.role == "Admin") {
+      this.admin = true;
+    } else {
+      (this.student = false),
+        (this.employer = false),
+        (this.applicant = false),
+        (this.admin = false);
+    }
+    
+  },
   computed: {
     isLoggin() {
       return this.$store.state.is_logged;
     },
     MENUS() {
-      if (this.user.role == "Employeer") {
-        return this.employeer_menu;
-      } else if (this.user.role == "Student") {
-        return this.student_menu;
-      } else if (this.user.role == "Jobseeker") {
+      if (this..exists()) {
+        if (this.user.role == "Employeer") {
+          return this.employeer_menu;
+        } else if (this.user.role == "Student") {
+          return this.student_menu;
+        } else if (this.user.role == "Jobseeker") {
+          return this.seeker_menu;
+        }
+        return this.admin_menu;
+      } else {
         return this.seeker_menu;
+        
       }
-      return this.admin_menu;
     },
     currentRouteName() {
       return this.$route.name;
@@ -218,6 +66,7 @@ export default {
       return null;
     },
   },
+
   methods: {
     ...mapMutations(["setNavBar", "setOpenMachine", "setLoggedIn"]),
     togleDrawer() {
@@ -237,13 +86,5 @@ export default {
 };
 </script>
 <style>
-
-  @media screen and (min-width: 600px) {
-    .hidden-md-and-up{
-      content-visibility: hidden;
-    }
-  }
-
-
 </style>
  
--- src/views/Student.vue
+++ src/views/Student.vue
@@ -1,137 +1,186 @@
 <template>
-  <v-container>
-    <v-row>
-      <v-col cols="12">
-        <v-card>
-          <v-card-text>
-            <div style="float: left" class="mr-8">
-              <img :src="$store.state.systemlogo" width="110px" />
-            </div>
-            <div style="min-height: 100px">
-              <div class="text-h5 font-weight-bold text-primary">Welcome</div>
-              <div>Welcome to Tagum City - PEESO Program</div>
-            </div>
-          </v-card-text>
-        </v-card>
-      </v-col>
-    </v-row>
-    <v-row>
-      <v-col cols="6" md="4">
-        <v-card
-          color="primary"
-          dark
-          @click="$router.push({ path: 'programview' })"
-        >
-          <v-card-text>
-            <v-row>
-              <v-col cols="4" class="text-center">
-                <img :src="$store.state.img_dir + 'program.png'" width="48" />
-              </v-col>
-              <v-col cols="8">
-                <div class="text-h6">{{ programs.length }}</div>
-                Programs
-              </v-col>
-            </v-row>
-          </v-card-text>
-          <v-card
-            height="70"
-            width="100%"
-            class="pa-2"
-            align="right"
-            style="background: white; color: green"
-            @click="$router.push({ path: 'ScholarshipApplied' })"
-          >
-            <h4 class="ma-1">
-              <v-icon style="color: green" right size="20">
-                mdi-post-outline
-              </v-icon>
-              224
-            </h4>
-            <p class="ma-2">Applied Program</p>
+  <v-app>
+    <!-- <core-student-toolbar /> -->
+    <v-container>
+      <v-row>
+        <v-col cols="12">
+          <v-card>
+            <v-card-text>
+              <div style="float: left" class="mr-8">
+                <img :src="$store.state.systemlogo" width="110px" />
+              </div>
+              <div style="min-height: 100px">
+                <div class="text-h5 font-weight-bold text-primary">Welcome</div>
+                <div>Welcome to Tagum City - PEESO Program</div>
+              </div>
+            </v-card-text>
           </v-card>
-        </v-card>
-      </v-col>
-
-      <v-col cols="6" md="4">
-        <v-card
-          color="primary"
-          dark
-          @click="$router.push({ path: 'trainingview' })"
-        >
-          <v-card-text>
-            <v-row>
-              <v-col cols="4" class="text-center">
-                <img :src="$store.state.img_dir + 'training.png'" width="48" />
-              </v-col>
-              <v-col cols="8">
-                <div class="text-h6">{{ trainings.length }}</div>
-                Trainings
-              </v-col>
-            </v-row>
-          </v-card-text>
+        </v-col>
+      </v-row>
+      <v-row>
+        <v-col cols="6" md="4">
           <v-card
-            height="70"
-            width="100%"
-            class="pa-2"
-            align="right"
-            style="background: white; color: green"
-            @click="$router.push({ path: 'ScholarshipApplied' })"
+            color="primary"
+            dark
+            @click="$router.push({ path: 'programview' })"
           >
-            <h4 class="ma-1">
-              <v-icon style="color: green" right size="20">
-                mdi-post-outline
-              </v-icon>
-              224
-            </h4>
-            <p class="ma-2">Applied Training</p>
+            <v-card-text>
+              <v-row>
+                <v-col cols="4" class="text-center">
+                  <img :src="$store.state.img_dir + 'program.png'" width="48" />
+                </v-col>
+                <v-col cols="8">
+                  <div class="text-h6">{{ programs.length }}</div>
+                  Programs
+                </v-col>
+              </v-row>
+            </v-card-text>
+            <v-card
+              height="70"
+              width="100%"
+              class="pa-2"
+              align="right"
+              style="background: white; color: green"
+              @click="$router.push({ path: 'ScholarshipApplied' })"
+            >
+              <h4 class="ma-1">
+                <v-icon style="color: green" right size="20">
+                  mdi-post-outline
+                </v-icon>
+                224
+              </h4>
+              <p class="ma-2">Applied Program</p>
+            </v-card>
           </v-card>
-        </v-card>
-      </v-col>
+        </v-col>
 
-      <v-col cols="12" md="4">
-        <v-card color="primary" dark @click="$router.push({ path: 'jobview' })">
-          <v-card-text>
-            <v-row>
-              <v-col cols="4" class="text-center">
-                <img :src="$store.state.img_dir + 'job.png'" width="48" />
-              </v-col>
-              <v-col cols="8">
-                <div class="text-h6">{{ jobs.length }}</div>
-                Job Posts
-              </v-col>
-            </v-row>
-          </v-card-text>
+        <v-col cols="6" md="4">
           <v-card
-            height="70"
-            width="100%"
-            class="pa-2"
-            align="right"
-            style="background: white; color: green"
-            @click="$router.push({ path: 'ScholarshipApplied' })"
+            color="primary"
+            dark
+            @click="$router.push({ path: 'trainingview' })"
           >
-            <h4 class="ma-1">
-              <v-icon style="color: green" right size="20">
-                mdi-post-outline
-              </v-icon>
-              224
-            </h4>
-            <p class="ma-2">Applied Jobs</p>
+            <v-card-text>
+              <v-row>
+                <v-col cols="4" class="text-center">
+                  <img
+                    :src="$store.state.img_dir + 'training.png'"
+                    width="48"
+                  />
+                </v-col>
+                <v-col cols="8">
+                  <div class="text-h6">{{ trainings.length }}</div>
+                  Trainings
+                </v-col>
+              </v-row>
+            </v-card-text>
+            <v-card
+              height="70"
+              width="100%"
+              class="pa-2"
+              align="right"
+              style="background: white; color: green"
+              @click="$router.push({ path: 'ScholarshipApplied' })"
+            >
+              <h4 class="ma-1">
+                <v-icon style="color: green" right size="20">
+                  mdi-post-outline
+                </v-icon>
+                224
+              </h4>
+              <p class="ma-2">Applied Training</p>
+            </v-card>
           </v-card>
-        </v-card>
-      </v-col>
-    </v-row>
+        </v-col>
 
-    <v-row>
-      <v-col cols="12" md="8">
-        <v-card>
-          <v-card class="mb-4">
-            <v-card-title>
-              <span class="text-primary">LATEST PROGRAM</span>
-            </v-card-title>
+        <v-col cols="12" md="4">
+          <v-card
+            color="primary"
+            dark
+            @click="$router.push({ path: 'jobview' })"
+          >
+            <v-card-text>
+              <v-row>
+                <v-col cols="4" class="text-center">
+                  <img :src="$store.state.img_dir + 'job.png'" width="48" />
+                </v-col>
+                <v-col cols="8">
+                  <div class="text-h6">{{ jobs.length }}</div>
+                  Job Posts
+                </v-col>
+              </v-row>
+            </v-card-text>
+            <v-card
+              height="70"
+              width="100%"
+              class="pa-2"
+              align="right"
+              style="background: white; color: green"
+              @click="$router.push({ path: 'ScholarshipApplied' })"
+            >
+              <h4 class="ma-1">
+                <v-icon style="color: green" right size="20">
+                  mdi-post-outline
+                </v-icon>
+                224
+              </h4>
+              <p class="ma-2">Applied Jobs</p>
+            </v-card>
+          </v-card>
+        </v-col>
+      </v-row>
+
+      <v-row>
+        <v-col cols="12" md="8">
+          <v-card>
+            <v-card class="mb-4">
+              <v-card-title>
+                <span class="text-primary">LATEST PROGRAM</span>
+              </v-card-title>
+              <v-card-text>
+                <v-list three-line>
+                  <template v-for="(item, index) in programs">
+                    <v-list-item :key="index + '-programs'">
+                      <v-list-item-avatar tile size="62">
+                        <v-img
+                          :src="
+                            item.medias != null ? item.medias.logo : noImage
+                          "
+                        />
+                      </v-list-item-avatar>
+
+                      <v-list-item-content>
+                        <v-list-item-title
+                          ><a
+                            @click="
+                              $router.push('post/' + item.id).catch((err) => {})
+                            "
+                            >{{ item.title }}</a
+                          >
+                          <span class="text-caption">{{ item.status }}</span>
+                        </v-list-item-title>
+                        <v-list-item-subtitle v-html="item.subtitle" />
+                      </v-list-item-content>
+                    </v-list-item>
+                  </template>
+                </v-list>
+              </v-card-text>
+            </v-card>
+          </v-card>
+        </v-col>
+        <v-col cols="12" md="4">
+          <div class="mb-4">
+            <widget-training-widget></widget-training-widget>
+          </div>
+          <v-card>
+            <v-toolbar dense elevation="0">
+              <span class="text-primary">LATEST JOBS</span>
+              <v-spacer />
+            </v-toolbar>
             <v-card-text>
               <v-list three-line>
-                <template v-for="(item, index) in programs">
-                  <v-list-item :key="index + '-programs'">
+                <template v-for="(item, index) in jobs">
+                  <v-list-item :key="index + '-job'" v-if="index <= 4">
                     <v-list-item-avatar tile size="62">
                       <v-img
                         :src="item.medias != null ? item.medias.logo : noImage"
@@ -145,62 +194,25 @@
                             $router.push('post/' + item.id).catch((err) => {})
                           "
                           >{{ item.title }}</a
-                        >
-                        <span class="text-caption">{{ item.status }}</span>
-                      </v-list-item-title>
-                      <v-list-item-subtitle v-html="item.subtitle" />
+                        ></v-list-item-title
+                      >
+                      <v-list-item-subtitle>
+                        <em class="text-info">{{
+                          $moment(item.created_dt).startOf("day").fromNow()
+                        }}</em>
+                        - {{ item.post_meta.company }} -
+                        {{ item.post_meta.company_address }}
+                      </v-list-item-subtitle>
                     </v-list-item-content>
                   </v-list-item>
                 </template>
               </v-list>
             </v-card-text>
           </v-card>
-        </v-card>
-      </v-col>
-      <v-col cols="12" md="4">
-        <div class="mb-4">
-          <widget-training-widget></widget-training-widget>
-        </div>
-        <v-card>
-          <v-toolbar dense elevation="0">
-            <span class="text-primary">LATEST JOBS</span>
-            <v-spacer />
-          </v-toolbar>
-          <v-card-text>
-            <v-list three-line>
-              <template v-for="(item, index) in jobs">
-                <v-list-item :key="index + '-job'" v-if="index <= 4">
-                  <v-list-item-avatar tile size="62">
-                    <v-img
-                      :src="item.medias != null ? item.medias.logo : noImage"
-                    />
-                  </v-list-item-avatar>
-
-                  <v-list-item-content>
-                    <v-list-item-title
-                      ><a
-                        @click="
-                          $router.push('post/' + item.id).catch((err) => {})
-                        "
-                        >{{ item.title }}</a
-                      ></v-list-item-title
-                    >
-                    <v-list-item-subtitle>
-                      <em class="text-info">{{
-                        $moment(item.created_dt).startOf("day").fromNow()
-                      }}</em>
-                      - {{ item.post_meta.company }} -
-                      {{ item.post_meta.company_address }}
-                    </v-list-item-subtitle>
-                  </v-list-item-content>
-                </v-list-item>
-              </template>
-            </v-list>
-          </v-card-text>
-        </v-card>
-      </v-col>
-    </v-row>
-  </v-container>
+        </v-col>
+      </v-row>
+    </v-container>
+  </v-app>
 </template>
   <script>
 import { mapMutations } from "vuex";
