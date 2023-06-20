a32e72421122c39ea4537703c9eca2257bc6022a
Joemarie123 <jomre27@gmail.com>
2023-06-20 14:42:01 +0800

    Updated With Table Passing

--- src/views/Viewing/TablePassingData.vue
+++ src/views/Viewing/TablePassingData.vue
@@ -1,140 +1,3 @@
-<!--  
-<template>
-    <div>
-        <v-container class="bg-surface-variant">
-            <v-row no-gutters>
-                <v-col order="12" style="border-style: solid;border-width: 2px; border-radius: 13px;"> <v-toolbar flat>
-                        <v-spacer></v-spacer> <v-btn color="primary" @click="moveToDestination">
-                            Give schedule
-                        </v-btn>
-                    </v-toolbar>
-                    <v-sheet class="pa-2 ma-2">
-                        <v-text-field v-model="search" append-icon="mdi-magnify" label="Search" single-line hide-details
-                            style="margin-left: 50%;margin-bottom: 5%;"></v-text-field>
-                        <v-data-table :headers="sourceHeaders" :items="sourceData" item-key="id" :selectable="true"
-                            :search="search" v-model="selectedItems">
-
-                            <template v-slot:top>
-
-                            </template>
-                            <template v-slot:item="{ item }">
-                                <tr>
-                                    <td>
-                                        <v-checkbox v-model="selectedItems" :value="item"></v-checkbox>
-                                    </td>
-                                    <td>{{ item.name }}</td>
-                                    <td>{{ item.email }}</td>
-                                    <td>{{ item.age }}</td>
-                                    <td>{{ item.address }}</td>
-
-                                </tr>
-                            </template>
-                        </v-data-table>
-                    </v-sheet>
-                </v-col>
-                <v-col order="12" class="mt-16"
-                    style="border-style: solid;border-width: 2px; border-radius: 13px; margin-left: 1.6%;">
-                    <v-sheet class="pa-2 ma-2">
-                        <v-data-table :headers="destinationHeaders" :items="destinationData" item-key="id">
-                            <template v-slot:item="{ item }">
-                                <tr>
-                                    <td>{{ item.name }}</td>
-                                    <td>{{ item.email }}</td>
-                                    <td>{{ item.age }}</td>
-                                    <td>{{ item.address }}</td>
-
-                                </tr>
-                            </template>
-                        </v-data-table>
-                    </v-sheet>
-                </v-col>
-
-            </v-row>
-        </v-container>
-    </div>
-</template>
-  
-<script>
-export default {
-    data() {
-
-        return {
-            search: '',
-            sourceHeaders: [
-                { text: "Select", value: "select", sortable: false },
-                { text: 'Name', value: 'name' },
-                { text: 'Contact No.', value: 'email' },
-                { text: 'Age', value: 'age' },
-                { text: 'Address', value: 'address' },
-
-            ],
-            sourceData: [
-                { name: 'John Doe', email: '09123456789', age: '23', address: 'address1' },
-                { name: 'Jane Smith', email: '09123456789', age: '24', address: 'address1' },
-                { name: '  Smith', email: '09123456789', age: '25', address: 'address1' },
-                { name: 'Jane  ', email: '09123456789', age: '25', address: 'address1' },
-                { name: 'Albert  ', email: '09123456789', age: '25', address: 'address1' },
-                { name: 'Anne', email: '09123456789', age: '25', address: 'address1' },
-
-            ],
-            destinationHeaders: [
-                { text: 'Name', value: 'name' },
-                { text: 'Contact No.', value: 'email' },
-                { text: 'Age', value: 'age' },
-                { text: 'Address', value: 'address' },
-
-            ],
-            destinationData: [],
-            selectedItems: [],
-        };
-    },
-    methods: {
-        moveToDestination() {
-            this.destinationData = this.destinationData.concat(this.selectedItems);
-            this.sourceData = this.sourceData.filter(
-                (item) => !this.selectedItems.includes(item)
-            );
-            this.selectedItems = [];
-        },
-    },
-};
-</script> -->
-
-<!-- 
-<template>
-    <div>
-        <v-data-table :headers="tableHeaders" :items="tableData" item-key="id" @click:row="selectTableRow"></v-data-table>
-
-        <v-text-field v-model="selectedItemValue" label="Selected Item"></v-text-field>
-    </div>
-</template>
-  
-<script>
-export default {
-    data() {
-        return {
-            tableHeaders: [
-                { text: "ID", value: "id" },
-                { text: "Name", value: "name" },
-                // Add more headers as needed
-            ],
-            tableData: [
-                { id: 1, name: "Item 1" },
-                { id: 2, name: "Item 2" },
-                // Add more data rows as needed
-            ],
-            selectedItemValue: "",
-        };
-    },
-    methods: {
-        selectTableRow(item) {
-            this.selectedItemValue = item.name;
-        },
-    },
-};
-</script> -->
-  
-
 
 <template>
     <div>
@@ -144,23 +7,29 @@ export default {
                     <v-sheet class="pa-2 ma-2">
                         <v-text-field v-model="search" append-icon="mdi-magnify" label="Search" single-line hide-details
                             style="margin-left: 50%;margin-bottom: 5%;"></v-text-field>
-                        <v-data-table @click:row="selectRow" :headers="headers" :items="data"  :search="search"
+                        <v-data-table @click:row="selectRow" item-key="name" single-select :headers="headers" :items="data"  :search="search"
                             >
                             <template v-slot:top>
                     
                             </template>
                             
-                        <!--     <template v-slot:item="{ item }">
+                           <!--  <template v-slot:item="{ item }">
                                 <tr  @click="passData">
                                     <td>
-                                        <v-checkbox v-model="selectedItems" :value="item" @click="passData"></v-checkbox>
+                                        <v-checkbox v-model="selectedItems" :value="item" ></v-checkbox>
                                     </td>
                                     <td @click="passData">{{ item.name }}</td>
                                     <td @click="passData">{{ item.contact }}</td>
 
                                 </tr>
+                            </template> -->
+
+                            <template slot="item.switch1" slot-scope="{ item }">
+                        <v-checkbox v-model="item.switch1" color="black" dense></v-checkbox>
                             </template>
- -->
+
+
+
 
                         </v-data-table>
                         <v-btn color="primary" @click="passData">Send invite</v-btn>
@@ -192,13 +61,13 @@ export default {
                             </v-col>
                             <v-col order="12">
                                 <v-sheet class="pa-2 ma-2" style="margin-top: 6%;">
-                                    <v-text-field label="Ethnic Group:" readonly></v-text-field>
-                                    <v-text-field label="Address:" readonly></v-text-field>
-                                    <v-text-field v-model="textField2" label="Contact No.:" readonly></v-text-field>
-                                    <v-text-field label="Email:" readonly></v-text-field>
-                                    <v-text-field label="Type of ID:" readonly></v-text-field>
-                                    <v-text-field label="SSS:" readonly></v-text-field>
-                                    <v-text-field label="Tin:" readonly></v-text-field>
+                                    <v-text-field v-model="Text_EthnicGroup" label="Ethnic Group:" readonly></v-text-field>
+                                    <v-text-field v-model="Text_Address"  label="Address:" readonly></v-text-field>
+                                    <v-text-field v-model="Text_ContactNo" label="Contact No.:" readonly></v-text-field>
+                                    <v-text-field v-model="Text_Email" label="Email:" readonly></v-text-field>
+                                    <v-text-field v-model="Text_TypeofID" label="Type of ID:" readonly></v-text-field>
+                                    <v-text-field v-model="Text_SSS" label="SSS:" readonly></v-text-field>
+                                    <v-text-field  v-model="Text_TIN" label="Tin:" readonly></v-text-field>
                                 </v-sheet>
                             </v-col>
                         </v-row>
@@ -210,17 +79,17 @@ export default {
                             <v-col order="12">
                                 <v-sheet class="pa-2 ma-2">
                                     <v-sheet class="pa-2 ma-2">
-                                        <v-text-field label="Employment Status:" readonly></v-text-field>
-                                        <v-text-field label="Employment Type:" readonly></v-text-field>
-                                        <v-text-field label="PESLA Attendance:" readonly></v-text-field>
+                                        <v-text-field v-model="Text_EmployementStatus" label="Employment Status:" readonly></v-text-field>
+                                        <v-text-field v-model="Text_EmployementType" label="Employment Type:" readonly></v-text-field>
+                                        <v-text-field v-model="Text_peslaattendance" label="PESLA Attendance:" readonly></v-text-field>
                                     </v-sheet>
                                 </v-sheet>
                             </v-col>
                             <v-col order="12">
                                 <v-sheet class="pa-2 ma-2" style="margin-top: 6%;">
-                                    <v-text-field label="Former OFW:" readonly></v-text-field>
-                                    <v-text-field label="4ps Beneficiary:" readonly></v-text-field>
-                                    <v-text-field label="First Time Job Seeker:" readonly></v-text-field>
+                                    <v-text-field v-model="Text_formerofw" label="Former OFW:" readonly></v-text-field>
+                                    <v-text-field v-model="Text_Four_pesbeneficiary" label="4ps Beneficiary:" readonly></v-text-field>
+                                    <v-text-field v-model="Text_firsttimejobseekers" label="First Time Job Seeker:" readonly></v-text-field>
                                 </v-sheet>
                             </v-col>
                         </v-row>
@@ -238,6 +107,7 @@ export default {
 export default {
     data() {
         return {
+            selectedId: -1,
             search: '',
 
             Text_fullname:'',
@@ -248,8 +118,23 @@ export default {
             Text_Religion:'',
             Text_Disability:'',
 
+            Text_EthnicGroup:'',
+            Text_Address:'',
+            Text_ContactNo:'',
+            Text_Email:'',
+            Text_TypeofID:'',
+            Text_SSS:'',
+            Text_TIN:'',
+
+            Text_EmployementStatus:'',   
+            Text_EmployementType:'',
+            Text_peslaattendance:'',
+            Text_formerofw:'',
+            Text_Four_pesbeneficiary:'',
+            Text_firsttimejobseekers:'',
+
             headers: [
-                { text: "Select", value: "select", sortable: false },
+                { text: "Select", value: "switch1", sortable: false },
                 { text: "Name", value: "name" },
                 { text: "Contact", value: "contact" },
 
@@ -298,7 +183,7 @@ TypeofID:'Drivers License',
 Religion:'Chatolic',
 SSS: '2234-4213-5135',
 Disability:'None',
-Tin:'4421-4421-512',
+Tin:'2242-2211-552',
 
 EmployementStatus:'Self-Employed',
 formerofw:'India',
@@ -323,9 +208,9 @@ Email: 'Pablo123@gmail.com',
 Sex:'Male',
 TypeofID:'LET License',
 Religion:'Jehovas withness',
-SSS: '2234-4213-5135',
+SSS: '4452-552-7786',
 Disability:'None',
-Tin:'4421-4421-512',
+Tin:'7754-7743-7783',
 
 EmployementStatus:'Un-Employed',
 formerofw:'Ameria',
@@ -335,6 +220,7 @@ peslaattendance:'Yes',
 firsttimejobseekers:'Yes'
 
 },
+
 
 { 
 
@@ -366,20 +252,20 @@ firsttimejobseekers:'Yes'
 { 
 
 id: 1, 
-name: "Pablo Escobar", 
-Ethnicgroup: 'Mexicano',
-Dateofbirth: 'September 2,1995',
-Address:'Capital City of Mexico',
-Age:'45',
-contact: '094894651',
-CivilStatus: 'Single',    
-Email: 'escobar123@gmail.com',
+name: "Jograd Mahusay", 
+Ethnicgroup: 'BIsaya',
+Dateofbirth: 'April 5,1995',
+Address:'Santol Tagum City',
+Age:'33',
+contact: '0954566',
+CivilStatus: 'Marrie',    
+Email: 'mahusay123@gmail.com',
 Sex:'Male',
-TypeofID:'License To Kill',
-Religion:'Kulto 45',
-SSS: '22441-5531-3341',
+TypeofID:'Drivers License',
+Religion:'Roman Chatolic',
+SSS: '4451-33341-55536',
 Disability:'memory loss',
-Tin:'6657-5553-7778',
+Tin:'7742-8865-9986',
 
 EmployementStatus:'Self-Employed',
 formerofw:'Manila ',
@@ -390,7 +276,6 @@ firsttimejobseekers:'Yes'
 
 },
 
-
             ],
           /*   selectedItems: [],
             textField1: "",
@@ -399,7 +284,18 @@ firsttimejobseekers:'Yes'
         };
     },
     methods: {
-        selectRow(item) {
+
+    rowClick: function (item, row) {      
+      row.select(true);
+      this.selectedId=item.name
+    },
+
+        selectRow: function (item,row) {
+
+            row.select(true);
+      this.selectedId=item.name
+
+
       this.Text_fullname = item.name; // Assign the clicked row's name to selectedRowData
       this.Text_dateofbirth = item.Dateofbirth;
       this.Text_age = item.Age;
@@ -407,6 +303,24 @@ firsttimejobseekers:'Yes'
       this.Text_Sex = item.Sex;
       this.Text_Religion = item.Religion;
       this.Text_Disability = item.Disability;
+
+      this.Text_EthnicGroup = item.Ethnicgroup; 
+      this.Text_Address = item.Address;
+      this.Text_ContactNo = item.contact;
+      this.Text_Email = item.Email;
+      this.Text_TypeofID = item.TypeofID;
+      this.Text_SSS = item.SSS;   
+      this.Text_TIN = item.Tin;   
+
+      this.Text_EmployementStatus = item.EmployementStatus;
+      this.Text_EmployementType = item.EmployementType;
+      this.Text_peslaattendance = item.peslaattendance;
+      this.Text_formerofw = item.formerofw;
+      this.Text_Four_pesbeneficiary = item.Four_pesbeneficiary;
+      this.Text_firsttimejobseekers = item.firsttimejobseekers;
+
+
+
     },
 
     
@@ -422,3 +336,10 @@ firsttimejobseekers:'Yes'
 };
 </script>
   
+<style>
+
+tr.v-data-table__selected {
+  background: #4960d6c0 !important;
+}
+</style>
+
