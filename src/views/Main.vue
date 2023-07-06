30e131b37ce28b7685625ba49c642bf4eb5b845c
jogz44 <annjogz@gmail.com>
2023-07-06 14:35:29 +0800

    with homepage

--- src/views/Main.vue
+++ src/views/Main.vue
@@ -1,5 +1,143 @@
 <template>
   <v-container>
+
+    <v-row>
+      <v-col cols="12" md="8" class="backgruond"></v-col>
+      <v-col cols="12" md="4" style="">
+        <h3 class="mt-5 weare">#WEARE<span class="tagum">TAGUM</span></h3>
+
+        <p class="mt-5">
+          Public Education and Employment Services Office (P.E.E.S.O.) is a
+          government agency tasked in providing education and employment
+          services under the Department of Labor and Employment (DOLE).
+        </p>
+      </v-col>
+
+    </v-row>
+
+
+    <v-col cols="12" style="margin-top: 2%; margin-bottom: 2%;">
+      <v-card class="mx-auto" style="height: 101%;">
+        <v-card-text style="height: 77%;">
+          <p class="text-h4 weare">Accessibility and Inclusivity</p>
+          <div class="text--primary">
+            Regardless of a person's background or ability, we are committed to offering accessible and inclusive
+            services. We guarantee that everyone has an equal chance to succeed in the job market thanks to our
+            platform.
+          </div>
+        </v-card-text>
+        <v-card-actions>
+          <br />
+          <br />
+        </v-card-actions>
+        <v-dialog v-model="revealEmployer" max-width="600px">
+          <v-card class="transition-fast-in-fast-out v-card--reveal">
+            <v-card-text class="pb-0">
+              <p class="text-h5 weare">Student Access</p>
+              <p>
+                <b>Step 1: Create an Account</b><br />
+                - Look for the "Sign Up" or "Create Account" button and
+                click on it. <br />
+                - Fill out the registration form with your personal
+                information, such as your name, email address, and a
+                password.
+              </p>
+              <p>
+                <b>Step 2: Verify your email</b><br />
+                - Check your email inbox for a verification email from the
+                job application system. <br />
+                - Open the email and click on the verification link
+                provided.<br />
+                - This step confirms your email address and activates your
+                account.
+              </p>
+              <p>
+                <b>Step 3: 3: Navigate to your employer dashboard</b><br />
+                - Once logged in, you will be redirected to your employer
+                dashboard or a designated section for employers.<br />
+                - Navigate to the "Job Opportunities" or "Job Listings"
+                section. <br />
+                - Familiarize yourself with the available features and
+                options specific to employers, such as job postings and
+                candidate management
+              </p>
+              <p>
+                <b>Step 4: Create a job posting</b><br />
+                - Click on the "Post a Job" or "Create Job Listing" button
+                within your employer dashboard.<br />
+                - Provide the necessary details for the job posting,
+                including job title, description, qualifications, and any
+                other relevant information.<br />
+                - Set the desired application deadline and any other
+                specific requirements. <br />
+                - Review the job posting information for accuracy before
+                submitting it.
+              </p>
+              <p>
+                <b>Step 5: Manage candidate applications</b><br />
+                - View the list of candidates who have applied for your
+                job posting.<br />
+                - Click on individual candidate profiles to review their
+                application materials, such as resumes, cover letters, and
+                additional documents. <br />
+                - Use the provided tools to track, organize, and manage
+                candidate applications effectively.
+              </p>
+              <p>
+                <b>Step 6: Communicate with candidates</b><br />
+                - Send messages to candidates regarding application
+                status, interview invitations, or any other necessary
+                communication.<br />
+                - Respond promptly to candidate inquiries or requests for
+                additional information.
+              </p>
+              <p>
+                <b>Step 7: Shortlist and select candidates</b><br />
+                - Review the applications and assess candidates based on
+                the provided qualifications and requirements.<br />
+                - Select the candidates you would like to proceed with for
+                further evaluation or interviews.
+              </p>
+              <p>
+                <b>Step 8: Schedule interviews and assessments</b><br />
+                - Coordinate and schedule interviews or assessments with
+                the selected candidates.<br />
+              </p>
+            </v-card-text>
+            <v-card-actions class="pt-0">
+              <v-btn text color="green accent-4" @click="revealEmployer = false">
+                Close
+              </v-btn>
+            </v-card-actions>
+          </v-card>
+        </v-dialog>
+      </v-card>
+      <div>
+        <CarouselHomePage style="margin-bottom: 2%; cursor: pointer;" />
+
+      </div>
+    </v-col>
+
+    <v-row>
+      <v-col cols="4" md="4">
+
+        <EducationHome />
+      </v-col>
+
+
+
+
+      <v-col cols="4" md="4">
+
+        <ApplicantHome />
+      </v-col>
+      <v-col cols="4" md="4">
+
+        <TrainingHome />
+      </v-col>
+
+    </v-row>
+
     <v-row>
       <v-col cols="12" md="4">
         <div class="mx-auto" style="height: 101%;">
@@ -411,13 +549,9 @@ export default {
     this.jobposts();
     //this.trainingposts()
     this.programposts();
-    this.loadJobs();
-    this.initializeObserver();
   },
   methods: {
     ...mapMutations(["setLoggedIn", "setAppBar", "setMonthDailySales"]),
-
-    
     jobposts() {
       this.jobs = []
       this.$http
@@ -454,39 +588,6 @@ export default {
           console.log(e);
         });
     },
-    
-    initializeObserver() {
-      this.observer = new IntersectionObserver((entries) => {
-        entries.forEach((entry) => {
-          if (entry.isIntersecting) {
-            this.loadMoreJobs();
-          }
-        });
-      });
-
-      this.observer.observe(this.$refs.lazyLoader);
-    },
-
-    loadJobs() {
-      this.isLoading = true;
-
-      // Fetch initial set of jobs
-      this.$http
-        .post("post/list", { type: "job" })
-        .then((response) => {
-          if (response.data.status) {
-            this.jobs = response.data.posts;
-            
-            this.isLoading = false;
-          }
-        })
-        .catch((e) => {
-          console.log("output",e);
-          
-        });
-    },
-
-    
   },
 };
 </script>
